b9d88907ef852bad8e9363d20faa7ea28f69643a
Colin Nguyen <nguyen_c9@denison.edu>
2026-07-25 17:21:45 -0400

    Implement gender classifier with decision tree

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from sklearn import tree
 
 # Build a Decision Tree!
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38]]
+Y = ['male', 'female', 'female', 'female']
+
+dt = tree.DecisionTreeClassifier()
+
+dt = dt.fit(X,Y)
+
+prediction = clf.predict([190, 70, 43])
+
+print prediction
